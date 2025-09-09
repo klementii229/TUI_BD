c414cca57c74becb6c02fb459cf7ba62fdcf081b
klementii229 <dimaklejmenov417@gmail.com>
2025-09-09 21:58:43 +0300

    Удаление файлов от gitignotre

--- build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o.d
+++
@@ -1,4 +0,0 @@
-_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o: \
- /home/kernel/Desktop/TUIBD/build/_deps/ftxui-src/src/ftxui/dom/flexbox_config.cpp \
- /usr/include/stdc-predef.h \
- /home/kernel/Desktop/TUIBD/build/_deps/ftxui-src/include/ftxui/dom/flexbox_config.hpp
