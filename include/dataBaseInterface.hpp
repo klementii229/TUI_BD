4eabb928c2ddb22db8740f6f249c45a74c403f73
klementii229 <dimaklejmenov417@gmail.com>
2025-09-10 20:54:23 +0300

    Инициализация архитектуры

--- include/dataBaseInterface.hpp
+++ include/dataBaseInterface.hpp
@@ -3,27 +3,26 @@
 #include <vector>
 #include <soci/soci.h>
 
-// Наш собственный "легкий" тип для представления строки результата
+// Собственный "легкий" тип для представления строки результата
 using DatabaseRow = std::vector<std::string>;
-using DatabaseResult = std::vector<DatabaseRow>;
+using DatabaseResultTable = std::vector<DatabaseRow>;
 
 class IDatabaseConnector {
 public:
     virtual ~IDatabaseConnector() = default;
-
     // Чисто виртуальные методы, которые должны быть реализованы
     virtual bool Connect(const std::string& connectionString) = 0;
     virtual void Disconnect() = 0;
     virtual bool IsConnected() const = 0;
 
     // Выполнить запрос и получить результат (для SELECT)
-    virtual DatabaseResult ExecuteQuery(const std::string& query) = 0;
+    virtual DatabaseResultTable ExecuteQuery(const std::string& query) = 0;
     // Выполнить команду (INSERT, UPDATE, DELETE)
     virtual bool ExecuteCommand(const std::string& command) = 0;
 
     // Методы для получения метаданных (схемы БД)
     virtual std::vector<std::string> GetTableList() = 0;
-    virtual DatabaseResult GetTableSchema(const std::string& tableName) = 0;
+    virtual DatabaseResultTable GetTableSchema(const std::string& tableName) = 0;
 
     // Можно добавить метод для получения имени типа БД
     virtual std::string GetDatabaseType() const = 0;
